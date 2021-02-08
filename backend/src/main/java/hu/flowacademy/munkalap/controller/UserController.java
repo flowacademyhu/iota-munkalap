049b9d7984b2734b98fad2a359eb28530adae608
gyanyim <gyanyi.miklos@gmail.com>
2021-02-08 16:48:07 +0100

    Add User Controller package, and User Controller.

---
+++ backend/src/main/java/hu/flowacademy/munkalap/controller/UserController.java
@@ -0,0 +1,20 @@
+package hu.flowacademy.munkalap.controller;
+
+import hu.flowacademy.munkalap.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    
+}
