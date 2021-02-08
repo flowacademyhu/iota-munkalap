8b83d689eadcaeba2105ffa860ecc2362e5a5482
Aniko <s.aniko9424@gmail.com>
2021-02-08 19:17:37 +0100

    Import SaveEmployee and use bootstrap container

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import {Formik, Form} from 'formik';
+import SaveEmployee from './SaveEmployee';
+import './style.css';
 
 function App() {
   return (
-    <div className="App">
-      Hello
+    <div className="container my-5">
+      <div className="row justify-content-center">
+        <div className="col-12">
+          <SaveEmployee />
+        </div>
+      </div>
     </div>
   );
 }
