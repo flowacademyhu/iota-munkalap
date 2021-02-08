80b2e248862deaee3d15ef186869d610bd648736
Aniko <s.aniko9424@gmail.com>
2021-02-08 19:16:44 +0100

    Make SaveEmployee component with title and one input (name)

---
+++ frontend/src/SaveEmployee.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Formik, Form } from 'formik';
+import Input from './Input';
+
+function SaveEmployee() {
+  return (
+    <Formik 
+     // initialValues={{ name: '', email: '', password: '', password: '' }}
+    >
+      <Form>
+        <h1 className='text-center'>Új munkavállaló</h1>
+        <Input label='Név' type='text' />
+      </Form>
+    </Formik>
+  )
+}
+
+export default SaveEmployee;
