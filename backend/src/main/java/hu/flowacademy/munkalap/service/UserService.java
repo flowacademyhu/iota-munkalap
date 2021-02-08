d51a6fb8e52c020a2e97f577d310f7574409ebdc
gyanyim <gyanyi.miklos@gmail.com>
2021-02-08 16:31:17 +0100

    Service Save without Validation

---
+++ backend/src/main/java/hu/flowacademy/munkalap/service/UserService.java
@@ -0,0 +1,21 @@
+package hu.flowacademy.munkalap.service;
+
+import hu.flowacademy.munkalap.entity.User;
+import hu.flowacademy.munkalap.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+}
