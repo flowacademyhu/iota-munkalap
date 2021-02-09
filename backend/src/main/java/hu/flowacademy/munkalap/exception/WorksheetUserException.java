e3103f6710c426865e4d27be611dd83b45dcc903
gyanyim <gyanyi.miklos@gmail.com>
2021-02-09 15:03:13 +0100

    Added WorksheetUserException for uservalidation in progress.

---
+++ backend/src/main/java/hu/flowacademy/munkalap/exception/WorksheetUserException.java
@@ -0,0 +1,21 @@
+package hu.flowacademy.munkalap.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class WorksheetUserException extends Exception {
+    private HttpStatus httpStatus;
+
+    public WorksheetUserException(String message) {
+        this(message, HttpStatus.BAD_REQUEST);
+    }
+
+    public WorksheetUserException(String message, HttpStatus httpStatus) {
+        super(message);
+        this.httpStatus = httpStatus;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+}
--- backend/src/main/java/hu/flowacademy/munkalap/service/UserService.java
+++ backend/src/main/java/hu/flowacademy/munkalap/service/UserService.java
@@ -5,9 +5,12 @@ import hu.flowacademy.munkalap.enumCustom.Kind;
 import hu.flowacademy.munkalap.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
+import javax.xml.bind.ValidationException;
 import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.Objects;
 
 @Service
 public class UserService {
@@ -20,9 +23,31 @@ public class UserService {
     }
 
     public User saveUser(User user) {
+        Objects.requireNonNull(user);
+
+        validateUser(user);
+
         user.setKind(Kind.USER);
         user.setEnabled(true);
         user.setCreatedAt(LocalDateTime.now());
         return userRepository.save(user);
     }
+
+    private void validateUser(User user) {
+        if(!(StringUtils.hasText(user.getName()))) {
+            throw new ValidationException()
+        }
+
+    }
 }
+
+/* public Food createFood(Food food) throws ValidationException {
+        log.info("Creating adding food: {}", food);
+        if (!(StringUtils.hasText(food.getName()) && food.getPrice() >= 0)) {
+            throw new ValidationException("Price is negative or no Food Name", HttpStatus.BAD_REQUEST);
+        } else {
+            Food created = foodRepository.save(food);
+            log.info("Created food: {}", created);
+            return created;
+        }
+    }*/
