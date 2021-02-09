6b63bc64f2c864cd0b5d9f72903ad4ebcdef08e8
Aniko <s.aniko9424@gmail.com>
2021-02-09 16:18:05 +0100

    In pop up the 'ok' button take to the main path

--- frontend/src/PopUp.js
+++ frontend/src/PopUp.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Modal from "react-bootstrap/Modal";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Link } from 'react-router-dom';
 
 function PopUp({ body }) {
   return (
     <Modal show={true}>
       <Modal.Body>{body}</Modal.Body>
       <Modal.Footer>
-        <button>OK</button>
+        <Link to=''>
+          <button>OK</button>
+        </Link>
       </Modal.Footer>
     </Modal>
   );
