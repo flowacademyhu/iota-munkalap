38da7cf547df9cb78078a092db2f185ccc75d56d
Aniko <s.aniko9424@gmail.com>
2021-02-09 09:13:14 +0100

    See all input field

--- frontend/src/SaveEmployee.js
+++ frontend/src/SaveEmployee.js
@@ -10,6 +10,9 @@ function SaveEmployee() {
       <Form>
         <h1 className='text-center'>Új munkavállaló</h1>
         <Input name='name' label='Név' type='text' />
+        <Input name='email' label='E-mail' type='email' />
+        <Input name='password' label='Jelszó' type='password' />
+        <Input name='confirmPassword' label='Jelszó még egyszer' type='password' />
       </Form>
     </Formik>
   )
