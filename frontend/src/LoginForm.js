1e2df3fe6182b0a3a1a83af3a30c35cf956d8d58
Meli <bodzamelani@gmail.com>
2021-02-09 15:48:01 +0100

    Changed method from put to get. (axios-mock)

--- frontend/src/LoginForm.js
+++ frontend/src/LoginForm.js
@@ -24,7 +24,7 @@ const schema = yup.object().shape({
         .min(5, "A jelszó legalább 5 karakter legyen!")
 });
 
-mock.onPut("http://localhost:8080/api/users/login", { username: 'user12345', password: 'user12345' }).reply(204, { accessToken: '...' });
+mock.onGet("http://localhost:8080/api/users/login", { username: 'user12345', password: 'user12345' }).reply(204, { accessToken: '...' });
 
 export default function LoginForm() {
     return (
@@ -39,7 +39,7 @@ export default function LoginForm() {
                         validationSchema={schema}
                         onSubmit={async values => {
                             console.log("Küldés:", values);
-                            const result = await axios.put('http://localhost:8080/api/users/login', { username: 'user12345', password: 'user12345' });
+                            const result = await axios.get('http://localhost:8080/api/users/login', { username: 'user12345', password: 'user12345' });
                             console.log(result);
                         }}
                     >
