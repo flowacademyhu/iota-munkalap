2da91c16e1cb8ad79bcacd4947194721bfa3327f
Aniko <s.aniko9424@gmail.com>
2021-02-10 11:50:55 +0100

    Use the EmployeeForm in the SaveEmployee

--- frontend/src/SaveEmployee.js
+++ frontend/src/SaveEmployee.js
@@ -1,26 +1,6 @@
 import React, { useState } from 'react';
-import { Formik, Form } from 'formik';
-import * as yup from 'yup';
-import { Link } from 'react-router-dom';
-import Input from './Input';
-import Button from './Button';
-import PopUp from './PopUp';
 import { postUser } from './UserAPI';
-
-const schema = yup.object().shape({
-  email: yup
-    .string()
-    .required("Az email kötelező!")
-    .email("Nem megfelelő email cím!"),
-  password: yup
-    .string()
-    .required("A jelszó kötelező!")
-    .min(5, "Legalább 5 karakteres jelszó kell!"),
-  confirmPassword: yup
-    .string()
-    .required("Add meg a jelszót még egyszer!")
-    .oneOf([yup.ref("password")], "A két jelszó nem egyezik meg!")
-});
+import EmployeeForm from './EmployeeForm';
 
 function SaveEmployee() {
   const [sent, setSent] = useState(false);
@@ -43,45 +23,15 @@ function SaveEmployee() {
   }
 
   return (
-    <div className="container my-5">
-      <div className="row justify-content-center">
-        <div className="col-12">
-          {sent 
-            && <PopUp 
-                body={popUpMessage} 
-                sentSuccessfully={sentSuccessfully}
-                setSent={setSent} 
-              />}
-          <Formik
-            initialValues={{
-              name: '',
-              email: '',
-              password: '',
-              confirmPassword: ''
-            }}
-            validationSchema={schema}
-            onSubmit={values => {
-              postData(values);
-            }}
-          >
-            <Form>
-              <h1 className='text-center'>Új munkavállaló</h1>
-              <Input name='name' label='Név' type='text' />
-              <Input name='email' label='E-mail' type='email' />
-              <Input name='password' label='Jelszó' type='password' />
-              <Input name='confirmPassword' label='Jelszó még egyszer' type='password' />
-              <div className='buttons'>
-                <Link to='/employees'>
-                  <Button text='Mégse' />
-                </Link>
-                <Button text='Mentés' type='submit' />
-              </div>
-            </Form>
-          </Formik>
-        </div>
-      </div>
-    </div>
-  )
+    <EmployeeForm 
+      sent={sent} 
+      setSent={setSent} 
+      sentSuccessfully={sentSuccessfully} 
+      popUpMessage={popUpMessage} 
+      sendData={postData}
+      path='new'
+    />
+  );
 }
 
 export default SaveEmployee;
