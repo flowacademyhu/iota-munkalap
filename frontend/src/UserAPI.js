cc71453a36a9d84a98913eb4b86673fecf15d5d9
Meli <bodzamelani@gmail.com>
2021-02-10 09:40:05 +0100

    deleted emtpy rows

--- frontend/src/UserAPI.js
+++ frontend/src/UserAPI.js
@@ -1,4 +1,7 @@
 import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter'
+
+var mock = new MockAdapter(axios);
 
 const api = axios.create({
     baseURL: `https://jsonplaceholder.typicode.com/`,
@@ -10,6 +13,8 @@ function getUsers() {
 }
 
 function login(username, password) {
+mock.onPost().reply(200);
+
     return api
         .post(`/users`, {username, password});
 }
