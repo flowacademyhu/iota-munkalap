e7bd74ac5b10dab93ff81b0962f70bd341dc55bc
Aniko <s.aniko9424@gmail.com>
2021-02-10 11:54:51 +0100

    Create an UpdateEmployee component to update an employee

---
+++ frontend/src/UpdateEmployee.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import EmployeeForm from './EmployeeForm';
+import { putUser } from './UserAPI';
+
+function UpdateEmployee({id}) {
+  const [sent, setSent] = useState(false);
+  const [sentSuccessfully, setSentSuccessfully] = useState(false);
+  const [popUpMessage, setPopUpMessage] = useState('');
+
+  async function putData(values) {
+    try {
+      const response = await putUser(id, values);
+      if (response.status === 200) {
+        setPopUpMessage('Munkavállaló sikeresen módosítva');
+        setSentSuccessfully(true);
+      } else {
+        setPopUpMessage('A módosítás sikertelen');
+      }
+    } catch (error) {
+      setPopUpMessage('A módosítás sikertelen');
+    }
+    setSent(true);
+  }
+
+  return (
+    <EmployeeForm 
+      sent={sent} 
+      setSent={setSent} 
+      sentSuccessfully={sentSuccessfully} 
+      popUpMessage={popUpMessage} 
+      sendData={putData}
+      path='update'
+    />
+  );
+}
+
+export default UpdateEmployee;
