ae348c50e82a30be51037f9ecc0afd4b8b9c7cb1
gyanyim <gyanyi.miklos@gmail.com>
2021-02-11 15:46:00 +0100

    Added WorksheetUsernameTakenException.class in @ExceptionHandler, to CustomExceptionHandler

--- backend/src/main/java/hu/flowacademy/worksheet/exception/CustomExceptionHandler.java
+++ backend/src/main/java/hu/flowacademy/worksheet/exception/CustomExceptionHandler.java
@@ -16,13 +16,19 @@ public class CustomExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<Object> handleAllExceptions(Exception e) {
-        log.error("ExceptionHandler Message{}", e.getLocalizedMessage());
+        log.error("ExceptionHandler Message, from Exception class: {}", e.getLocalizedMessage());
         return new ResponseEntity<>(List.of(e.getLocalizedMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(ValidationException.class)
     public final ResponseEntity<Object> handleUserException(ValidationException ex) {
-        log.error("ExceptionHandler Message: {}", ex.getLocalizedMessage());
+        log.error("ExceptionHandler Message, from ValidationException class: {}", ex.getLocalizedMessage());
+        return new ResponseEntity<>(List.of(ex.getLocalizedMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(WorksheetUsernameTakenException.class)
+    public final ResponseEntity<Object> worksheetUserNameException(WorksheetUsernameTakenException ex) {
+        log.error("ExceptionHandler Message, from WorksheetUserameTakenException: {}", ex.getLocalizedMessage());
         return new ResponseEntity<>(List.of(ex.getLocalizedMessage()), HttpStatus.BAD_REQUEST);
     }
 }
