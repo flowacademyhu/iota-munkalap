fca8bf456af4f8702a6a6ff36380b46ad0bc8e64
Meli <bodzamelani@gmail.com>
2021-02-11 09:25:32 +0100

    useToken works.

--- frontend/src/hooks/useToken.js
+++ frontend/src/hooks/useToken.js
@@ -1,7 +1,7 @@
 import { useState} from 'react';
 
 export default function useToken() {
-  const getToken = () => {
+  function getToken () {
     const tokenStr = sessionStorage.getItem('token');
     const userToken = JSON.parse(tokenStr);
     return userToken?.token
@@ -9,10 +9,11 @@ export default function useToken() {
 
   const [token, setToken] = useState(getToken());
 
-  const saveToken = userToken => {
-    sessionStorage.setItem('token', JSON.stringify(userToken));
-    setToken(userToken.token);
+  function saveToken(token ) {
+    sessionStorage.setItem('token', JSON.stringify(token));
+    setToken(token);
   };
+
 
   return {
     setToken: saveToken,
