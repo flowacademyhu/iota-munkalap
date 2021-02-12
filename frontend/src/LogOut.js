58aa0aa2cfb8815a19b25ca4016bddf0466834ce
Meli <bodzamelani@gmail.com>
2021-02-12 09:41:19 +0100

    LogOut component created

---
+++ frontend/src/LogOut.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+import {
+    Redirect
+} from "react-router-dom";
+
+
+export default function LogOut({setToken}) {
+
+    const [loggedOut, setLoggedOut] = useState(false);
+
+    function logout() {
+        sessionStorage.clear('acces_token');
+        setLoggedOut(true);
+        setToken("");
+
+    };
+
+    if (loggedOut) {
+        return <Redirect to="/" push={true} />
+    }
+
+    return <span onClick={logout}>LogOut</span>;
+};
