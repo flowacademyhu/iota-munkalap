ea13e54458ce8e63f178ce56a12923c25873025f
Meli <bodzamelani@gmail.com>
2021-02-15 21:04:30 +0100

    added CreateWorkSheetForm component

--- frontend/src/worksheet/CreateWorkSheetForm.js
+++ frontend/src/worksheet/CreateWorkSheetForm.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Formik, Form } from 'formik';
-import * as yup from 'yup';
 import { Link } from 'react-router-dom';
 import Input from '../Input';
 import Button from '../Button';
 import PopUp from '../PopUp';
+import SelectInput from '../SelectInput'
+import getCurrentDate from './Date';
 
 function CreateWorkSheetForm({ sent, setSent, sentSuccessfully, popUpMessage, sendData, path,
   title, name, email }) {
+
+    const [value, setValue] = useState("alma");
+
+    // function handleChange(param) {
+    //   setValue(param);
+
+    // }
+
+
+  const itemList1 = [{ id: 1, label: "Telepítés", value: "installation" },
+  { id: 2, label: "Javítás", value: "repair" },
+  { id: 3, label: "Karbantartás", value: "maintenance" },
+  { id: 4, label: "Egyéb", value: "other" }]
+
+  const itemList2 = [{ id: 1, label: "Térítéses", value: "repayment" },
+  { id: 2, label: "Garanciális", value: "warranty" }]
+
+  const itemList3 = [{ id: 1, label: "Térítéses", value: "repayment" },
+  { id: 2, label: "Garanciális", value: "warranty" }]
+
+  const itemList4 = [{ id: 1, label: "Készpénz", value: "cash" },
+  { id: 2, label: "Átutalás", value: "transfer" }]
+
   return (
     <div className="container my-5">
       <div className="row justify-content-center">
@@ -23,13 +47,20 @@ function CreateWorkSheetForm({ sent, setSent, sentSuccessfully, popUpMessage, se
             initialValues={{
               partner: '',
               typeOfWork: '',
-              assetAccounting: '',
-              confirmPassword: '',
+              assetSettlement: '',
+              workingTimeAccounting: '',
+              numberOfEmployees: 0,
+              overheadHour: 0,
+              deliveryKm: 0,
+              accountSerialNumber: '',
               description: '',
               usedMaterial: '',
-
+              typeOfPayment: '',
+              date: '',
+              workerSignature: '',
+              proofOfEmployment: '',
+              status: '',
             }}
-            validationSchema={schema}
             onSubmit={values => {
               sendData(values);
             }}
@@ -37,9 +68,24 @@ function CreateWorkSheetForm({ sent, setSent, sentSuccessfully, popUpMessage, se
             <Form>
               <h1 className='text-center'>{title}</h1>
               <Input name='partner' label='Partner' type='text' />
-              <Input name='typeOfWork' label='Munkavégzés jellege' type='email' />
-              <Input name='password' label='Jelszó' type='password' />
-              <Input name='confirmPassword' label='Jelszó még egyszer' type='password' />
+              <SelectInput handleChange={setValue} name='typeOfWork' label='Munkavégzés jellege' container={itemList1} />
+              {value === 'other' &&
+              <Input name='typeOfWork' label='Egyéb' type='text' />}
+              <div>{value}</div>
+              <SelectInput  name='assetSettlement' label='Eszközök elszámolás módja' container={itemList2} />
+              <SelectInput name='workingTimeAccounting' label='Munkaidő elszámolás módja' container={itemList3} />
+              <Input name='numberOfEmployees' label='Létszám' type='number' />
+              <Input name='overHeadHour' label='Rezsióra' type='number' />
+              <Input name='delivery' label='Kiszállítás' type='number' />
+              <Input name='accountSerialNumber' label='A munkalaphoz tartozó számla sorszáma' type='text' />
+              <Input name='description' label='Elvégzett munka leírása' type='text' />
+              <Input name='usedMaterial' label='Felhasznált anyagok' type='text' />
+              <SelectInput name='typeOfPayment' label='Fizetés módja' container={itemList4} />
+              <div>Kelt: {getCurrentDate()} </div>
+              <div>Munkát végezte: IDE KELL E-ALÁÍRÁS</div>
+              <div>munkavégzést igazolja: IDE KELL MÉG EGY E-ALÁÍRÁS</div>
+
+
               <div className='buttons'>
                 <Link to='/employees'>
                   <Button text='Mégse' />
@@ -55,3 +101,4 @@ function CreateWorkSheetForm({ sent, setSent, sentSuccessfully, popUpMessage, se
 }
 
 export default CreateWorkSheetForm;
+
