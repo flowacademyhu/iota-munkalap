5462d35fb547b2c75fc3409ae6a09245e0680ae0
Meli <bodzamelani@gmail.com>
2021-02-15 21:05:10 +0100

    Added CreateWorkSheet component

---
+++ frontend/src/worksheet/CreateWorkSheet.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { postWorkSheet } from '../UserAPI';
+import CreateWorkSheetForm from './CreateWorkSheetForm';
+
+function CreateWorkSheet() {
+  const [sent, setSent] = useState(false);
+  const [sentSuccessfully, setSentSuccessfully] = useState(false);
+  const [popUpMessage, setPopUpMessage] = useState('');
+
+  async function postData(values) {
+    try {
+      const response = await postWorkSheet(values);
+      if (response.status === 201) {
+        setPopUpMessage('Munkalap sikeresen létrehozva');
+        setSentSuccessfully(true);
+      } else {
+        setPopUpMessage('A létrehozás sikertelen');
+      }
+    } catch (error) {
+      setPopUpMessage('A létrehozás sikertelen');
+    }
+    setSent(true);
+  }
+
+  return (
+    <CreateWorkSheetForm
+      sent={sent} 
+      setSent={setSent} 
+      sentSuccessfully={sentSuccessfully} 
+      popUpMessage={popUpMessage} 
+      sendData={postData}
+      path='new'
+      title='Új munkalap létrehozása'
+    />
+  );
+}
+
+export default CreateWorkSheet;
