7927fb452024cf395cf3966930ed62caeb1a892c
BigG <gabszon002@gmail.com>
2021-02-15 15:21:42 +0100

    Deleted unused imports

--- backend/src/main/java/hu/flowacademy/worksheet/service/UserService.java
+++ backend/src/main/java/hu/flowacademy/worksheet/service/UserService.java
@@ -8,7 +8,6 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.validator.routines.EmailValidator;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
