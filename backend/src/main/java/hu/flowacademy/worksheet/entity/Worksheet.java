4103bd92fa23a94e07f56135ca97812ed9591b9c
gyanyim <gyanyi.miklos@gmail.com>
2021-02-15 15:59:01 +0100

    Added Entity Worksheet with variables, and enums.

--- backend/src/main/java/hu/flowacademy/worksheet/entity/Worksheet.java
+++ backend/src/main/java/hu/flowacademy/worksheet/entity/Worksheet.java
@@ -1,12 +1,10 @@
 package hu.flowacademy.worksheet.entity;
 
 import hu.flowacademy.worksheet.enumCustom.AssetSettlement;
+import hu.flowacademy.worksheet.enumCustom.TypeOfPayment;
 import hu.flowacademy.worksheet.enumCustom.TypeOfWork;
 import hu.flowacademy.worksheet.enumCustom.WorkingTimeAccounting;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 
@@ -21,32 +19,39 @@ public class Worksheet {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    //@NonNull
     //private Partner partner; szükséges lesz hozzá egy Partner osztály
     @Enumerated(EnumType.STRING)
+    @NonNull
     private TypeOfWork typeOfWork;
     @Enumerated(EnumType.STRING)
+    @NonNull
     private AssetSettlement assetSettlement;
     @Enumerated(EnumType.STRING)
+    @NonNull
     private WorkingTimeAccounting workingTimeAccounting;
-
+    @NonNull
+    private int numberOfEmployees = 1;
+    @NonNull
+    private float overheadHour;
+    @NonNull
+    private float deliveryKm;
+    private String accountSerialNumber;
+    @NonNull
+    @Lob
+    private String description;
+    @NonNull
+    private String usedMaterial;
+    @NonNull
+    @Enumerated(EnumType.STRING)
+    private TypeOfPayment typeOfPayment;
+    @NonNull
+    private String date;
+    @NonNull
+    private String workerSignature;
+    @NonNull
+    private String proofOfEmployment;
 
 }
 
-/*
-ID
-Partner: név, cím, elérhetőség (ha lesz már törzsadat)
-
-
-Eszköz elszámolás módja: Térítéses, Garanciális
-Munkaidő elszámolás módja: Térítéses, Garanciális
-Létszám: int fő
-Rezsióra: float óra
-Kiszállíás: float km
-A munkalaphoz tartozó számla sorszáma: string
-Az elvégzett munka leírása: szabadszöveges
-Felhasznált anyagok: szabadszöveges
-Fizetés módja: Készpénz, Átutalás
-Kelt: date
-Munkát végezte: e-aláírás
-Munkavégzést igazolja: e-aláírás
-*/
+// Nem tudom, hogy az e-aláírások, hogyan lesznek tárolva, milyen típusban, egyelőre Stringnek állítottam!
