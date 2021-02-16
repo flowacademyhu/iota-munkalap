a634276f71fcf0a47098065c498626a7ed207426
Meli <bodzamelani@gmail.com>
2021-02-16 18:22:04 +0100

    created  apis

--- frontend/src/api/WorkSheetAPI.js
+++ frontend/src/api/WorkSheetAPI.js
@@ -1,19 +1,27 @@
 import getApi from './getApi';
 import MockAdapter from "axios-mock-adapter";
+import axios from "axios";
 
 let api = getApi();
+
+const api3 = axios.create({
+    baseURL: `https://reqres.in/api/`
+})
 
 var mock = new MockAdapter(api);
 
 mock.onPost("/worksheets").reply(200);
 
+
+mock.onGet("/worksheets").reply(305);
+
 function getWorkSheets() {
-    return api
+    return api3
         .get(`worksheets`);
 }
 
 function postWorkSheet(credentials) {
-    return api
+    return api3
         .post(`/worksheets`, credentials);
 }
 
