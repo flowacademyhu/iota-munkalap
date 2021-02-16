ddf1f8617261ee03bd0098541c4d6c9f9a551509
gyanyim <gyanyi.miklos@gmail.com>
2021-02-16 15:10:07 +0100

    Delete the comment, and change the nullable in @Column annotation.

--- backend/src/main/java/hu/flowacademy/worksheet/entity/Worksheet.java
+++ backend/src/main/java/hu/flowacademy/worksheet/entity/Worksheet.java
@@ -17,41 +17,41 @@ public class Worksheet {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "worksheet_id")
-    private Long id;
+    @Column(name = "worksheet_id", nullable = false)
+    private String id;
     @Column(name = "partner_id")
     private String partnerId; // FIXME Change to many to one in the future.
     @Enumerated(EnumType.STRING)
-    @Column(name = "type_of_work")
+    @Column(name = "type_of_work", nullable = false)
     private TypeOfWork typeOfWork;
     @Enumerated(EnumType.STRING)
-    @Column(name = "asset_settlement")
+    @Column(name = "asset_settlement", nullable = false)
     private AssetSettlement assetSettlement;
     @Enumerated(EnumType.STRING)
-    @Column(name = "working_time_accounting")
+    @Column(name = "working_time_accounting", nullable = false)
     private WorkingTimeAccounting workingTimeAccounting;
-    @Column(name = "number_of_employees", columnDefinition = "int default 1")
+    @Column(name = "number_of_employees", columnDefinition = "int default 1", nullable = false)
     private int numberOfEmployees;
-    @Column(name = "overhead_hour", columnDefinition = "float default 1.0")
+    @Column(name = "overhead_hour", columnDefinition = "float default 1.0", nullable = false)
     private float overheadHour;
-    @Column(name = "delivery_km")
+    @Column(name = "delivery_km", nullable = false)
     private float deliveryKm;
-    @Column(name = "account_serial_number", nullable = false) //NULLABLE JAVÍTÁS
+    @Column(name = "account_serial_number")
     private String accountSerialNumber;
     @Lob
-    @Column(name = "description")
+    @Column(name = "description", nullable = false)
     private String description;
-    @Column(name = "used_material")
+    @Column(name = "used_material", nullable = false)
     private String usedMaterial;
-    @Column(name = "type_of_payment")
+    @Column(name = "type_of_payment", nullable = false)
     @Enumerated(EnumType.STRING)
     private TypeOfPayment typeOfPayment;
-    @Column(name = "local_date_time")
+    @Column(name = "local_date_time", nullable = false)
     @JsonFormat(pattern = "yyyy.MM.dd HH:mm:ss")
     private LocalDateTime localDateTime;
-    @Column(name = "worker_signature")
+    @Column(name = "worker_signature", nullable = false)
     private String workerSignature;
-    @Column(name = "proof_of_employment")
+    @Column(name = "proof_of_employment", nullable = false)
     private String proofOfEmployment;
     @Enumerated(EnumType.STRING)
     private WorksheetStatus worksheetStatus;
