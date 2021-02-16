6cb739dfcf2bc0a712bbf7de10561ac7d656d5c5
Aniko <s.aniko9424@gmail.com>
2021-02-16 16:44:11 +0100

    Rearrange imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,11 @@
-import LoginForm from './LoginForm';
 import React from "react";
-import './style.css';
-import useToken from './hooks/useToken';
-import './App.css';
-import Page from './Page';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import LoginForm from './LoginForm';
+import useToken from './hooks/useToken';
+import Page from './Page';
+import './style.css';
 
 function App() {
-
   const { token, setToken } = useToken();
   if (!token) {
     return <LoginForm setToken={setToken} />
