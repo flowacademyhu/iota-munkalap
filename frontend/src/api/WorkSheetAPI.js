0cc01c3e785e9975a4f4f5f7f1b60addbdcdbf72
Aniko <s.aniko9424@gmail.com>
2021-02-16 12:07:43 +0100

    Mock getWorkSheets

---
+++ frontend/src/api/WorkSheetAPI.js
@@ -0,0 +1,24 @@
+import getApi from './getApi';
+import MockAdapter from "axios-mock-adapter";
+
+let api = getApi();
+var mock = new MockAdapter(api);
+
+const workSheets = [
+  { id: '1', creater: 'employee1', date: '2020.12.01.', partner: 'partner1', type: 'type1', state: 'state1' },
+  { id: '2', creater: 'employee2', date: '2020.08.11.', partner: 'partner2', type: 'type2', state: 'state2' },
+  { id: '3', creater: 'employee3', date: '2000.12.01.', partner: 'partner3', type: 'type3', state: 'state3' },
+  { id: '4', creater: 'employee4', date: '2010.08.11.', partner: 'partner4', type: 'type4', state: 'state4' }
+]
+
+mock.onGet("/worksheets").reply(200, workSheets);
+
+function getWorkSheets() {
+  return api
+      .get(`/worksheets`);
+}
+
+export {
+  getWorkSheets,
+};
+
---
+++ frontend/src/hooks/useWorkSheets.js
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react';
+import { getWorkSheets } from '../api/WorkSheetAPI';
+
+export default function useWorkSheets() {
+  const [workSheets, setWorkSheets] = useState();
+  useEffect(() => {
+    async function updateWorkSheets() {
+      const { data } = await getWorkSheets();
+      setWorkSheets(data);
+    }
+    updateWorkSheets();
+  }, []);
+  return {
+    workSheets
+  }
+}
