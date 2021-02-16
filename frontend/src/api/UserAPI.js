fe1c095ce411055783f04d8598e16edd395df5ba
Aniko <s.aniko9424@gmail.com>
2021-02-16 11:07:38 +0100

    Outsource the api settings to getApi and create api folder.

--- frontend/src/api/UserAPI.js
+++ frontend/src/api/UserAPI.js
@@ -1,20 +1,6 @@
-import axios from 'axios';
+import getApi from './getApi';
 
-const api = axios.create({
-    baseURL: `/api/`
-})
-
-api.interceptors.request.use(
-    request => {
-        const token = sessionStorage.getItem('token');
-        if (token) {
-            request.headers = {...request.headers,  Authorization: 'Bearer ' + token}
-        }
-        return request;
-    }, error => {
-        return Promise.reject(error);
-    }
-)
+let api = getApi();
 
 async function loginUser(credentials) {
     try {
