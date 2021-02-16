a1a3bafff6fccdcd0405198dc16a457874bb2e55
Meli <bodzamelani@gmail.com>
2021-02-16 15:05:30 +0100

    added formik to select Input

--- frontend/src/SelectInput.js
+++ frontend/src/SelectInput.js
@@ -1,20 +1,27 @@
 import React from "react";
 import Form from 'react-bootstrap/Form';
+import { useField } from "formik";
+
 
 export default function SelectInput({handleChange, label, container, ...props }) {
+    const [field, meta] = useField(props);
+    const { error, touched } = meta;
+    const showError = touched && error;
     
+    console.log(field);
+    
+
     return (
         <div className='form-group my-4'>
-            
                 <Form.Group controlId="exampleForm.SelectCustom">
                     <Form.Label>{label}</Form.Label>
-                    <Form.Control onChange={(event) => handleChange(event.target.value)} as="select" custom>
+                    <Form.Control  {...field} as="select" custom  className={`form-control ${showError ? "is-invalid" : ""}`}>
                         {container.map(oneItem => (
                             <option key={oneItem.id} value={oneItem.value}> {oneItem.label} </option>
                         ))}
                     </Form.Control>
+                    {showError && <div className="invalid-feedback">{error}</div>}
                 </Form.Group>
-            
         </div>
     )
 }
