a93fd79913ee70a90869f8ee50890ec6a3e392d3
Aniko <s.aniko9424@gmail.com>
2021-02-17 12:10:16 +0100

    Auto formatting

--- frontend/src/Page.js
+++ frontend/src/Page.js
@@ -1,9 +1,9 @@
 import React from "react";
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Redirect
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
 } from "react-router-dom";
 import TableListOfEmployees from './employees/TableListOfEmployees';
 import CreateEmployee from './employees/CreateEmployee';
@@ -14,46 +14,48 @@ import './style.css';
 import TableListOfWorkSheets from './worksheets/TableListOfWorkSheets';
 
 export default function Page() {
-    return (
-        <Router>
-            <header>
-                <div className="container p-0">
-                    <Header />
-                    <Menu />
-                </div>
-            </header>
+  return (
+    <Router>
+      <header>
+        <div className="container p-0">
+          <Header />
+          <Menu />
+        </div>
+      </header>
 
-            <main>
-                <Switch>
-                    <Route path='/employees/new'>
-                        <CreateEmployee />
-                    </Route>
-                    <Route path='/employees/update/:id'>
-                        <UpdateEmployee />
-                    </Route>
-                    <Route exact path="/employees">
-                        <TableListOfEmployees />
-                    </Route>
-                    <Route path="/partners">
-                        <Partners />
-                    </Route>
-                    <Route path="/worksheets">
-                        <TableListOfWorkSheets />
-                    </Route>
-                    <Route path='/'>
-                        <Redirect to='/partners' />
-                    </Route>
-                </Switch>
-            </main>
-        </Router>
-    );
+      <main>
+        <Switch>
+          <div className="container p-0">
+            <Route path='/employees/new'>
+              <CreateEmployee />
+            </Route>
+            <Route path='/employees/update/:id'>
+              <UpdateEmployee />
+            </Route>
+            <Route exact path="/employees">
+              <TableListOfEmployees />
+            </Route>
+            <Route path="/partners">
+              <Partners />
+            </Route>
+            <Route path="/worksheets">
+              <TableListOfWorkSheets />
+            </Route>
+            <Route path='/'>
+              <Redirect to='/partners' />
+            </Route>
+          </div>
+        </Switch>
+      </main>
+    </Router>
+  );
 }
 
 function Partners() {
-    return (
-        <div>
-            <h2>Partnerek</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h2>Partnerek</h2>
+    </div>
+  );
 }
 
