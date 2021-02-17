369c9458c120e18d8a913696b0ea268a72c049f3
Aniko <s.aniko9424@gmail.com>
2021-02-17 17:29:37 +0100

    Change style

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -7,7 +7,7 @@ import UserData from './UserData';
 
 function Header() {
   return (
-    <div className='container px-0 py-2 m-0'>
+    <div className='my-3'>
       <div className='row align-items-center m-0'>
         <div className='col p-0'>
           <img className='col-auto logo p-0 m-0' src={logo} alt='ingo stop logo' />
