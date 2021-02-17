e0e905601b22e04ae353e85a6fb13c75d03091c7
Meli <bodzamelani@gmail.com>
2021-02-17 11:10:21 +0100

    autoformat

--- frontend/src/SelectInput.js
+++ frontend/src/SelectInput.js
@@ -3,22 +3,22 @@ import Form from 'react-bootstrap/Form';
 import { useField } from "formik";
 
 
-export default function SelectInput({handleChange, label, container, ...props }) {
+export default function SelectInput({ handleChange, label, container, ...props }) {
     const [field, meta] = useField(props);
     const { error, touched } = meta;
-    const showError = touched && error;    
+    const showError = touched && error;
 
     return (
         <div className='form-group my-4'>
-                <Form.Group controlId="exampleForm.SelectCustom">
-                    <Form.Label>{label}</Form.Label>
-                    <Form.Control  {...field} as="select" custom  className={`form-control ${showError ? "is-invalid" : ""}`}>
-                        {container.map(oneItem => (
-                            <option key={oneItem.id} value={oneItem.value} > {oneItem.label} </option>
-                        ))}
-                    </Form.Control>
-                    {showError && <div className="invalid-feedback">{error}</div>}
-                </Form.Group>
+            <Form.Group controlId="exampleForm.SelectCustom">
+                <Form.Label>{label}</Form.Label>
+                <Form.Control  {...field} as="select" custom className={`form-control ${showError ? "is-invalid" : ""}`}>
+                    {container.map(oneItem => (
+                        <option key={oneItem.id} value={oneItem.value} > {oneItem.label} </option>
+                    ))}
+                </Form.Control>
+                {showError && <div className="invalid-feedback">{error}</div>}
+            </Form.Group>
         </div>
     )
 }
