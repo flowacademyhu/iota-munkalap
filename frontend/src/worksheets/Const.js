b319b7e159c96f44e4e11d9194f6f878a1f655ac
Meli <bodzamelani@gmail.com>
2021-02-17 16:50:00 +0100

    introduced constants path variables in Const.js

--- frontend/src/worksheets/Const.js
+++ frontend/src/worksheets/Const.js
@@ -1,3 +1,10 @@
 export const TYPE_OF_WORK = {
     OTHER: 'OTHER'
 }
+
+export const PATH_VARIABLES = {
+    BASEPATH_EMPLOYEE: 'employees',
+    ENDPATH1_EMPLOYEE: 'new',
+    BASEPATH_WORKSHEET: 'worksheets',
+    ENDPATH1_WORKSHEET: 'new',
+}
