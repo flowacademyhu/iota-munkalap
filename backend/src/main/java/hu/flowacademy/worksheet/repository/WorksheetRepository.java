7429eb7efe817b983a92acfd827c9cb7e4c30c8e
Mara Patrik <marapatgab@gmail.com>
2021-02-18 12:43:05 +0100

    added queryMethod for filtering by dates to Repository

--- backend/src/main/java/hu/flowacademy/worksheet/repository/WorksheetRepository.java
+++ backend/src/main/java/hu/flowacademy/worksheet/repository/WorksheetRepository.java
@@ -4,6 +4,10 @@ import hu.flowacademy.worksheet.entity.Worksheet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface WorksheetRepository extends JpaRepository<Worksheet, String> {
+    List<Worksheet>findByCreatedAtLessThanMaxTimeAndCreatedAtMoreThanMinTime(LocalDateTime maxTime, LocalDateTime minTime);
 }
