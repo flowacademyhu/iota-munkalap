dc9dddb90de3eba0afd2e451e939fd1cdce1ab88
Aniko <s.aniko9424@gmail.com>
2021-02-18 20:17:49 +0100

    Change GetDecodedToken to a hook and remove to the hooks map. Change name to family_name + given_name.

---
+++ frontend/src/hooks/useCurrentUser.js
@@ -0,0 +1,19 @@
+import { useJwt } from 'react-jwt'
+import useToken from './useToken'
+
+function useCurrentUser() {
+  const { token } = useToken()
+  const { decodedToken } = useJwt(token)
+  const name = decodedToken?.family_name + ' ' + decodedToken?.given_name
+  const email = decodedToken?.email;
+  const isAdmin = decodedToken?.resource_access.worksheetclient.roles.includes(
+    'admin'
+  )
+  return {
+    name,
+    email,
+    isAdmin,
+  }
+}
+
+export default useCurrentUser;
