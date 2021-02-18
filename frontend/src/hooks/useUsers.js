7f028127edff743837041bfcc0ebc2fb0c5dc13d
Meli <bodzamelani@gmail.com>
2021-02-18 15:05:16 +0100

    added keyword in params

--- frontend/src/hooks/useUsers.js
+++ frontend/src/hooks/useUsers.js
@@ -2,16 +2,19 @@ import { useState, useEffect } from 'react'
 import { getUsers } from '../api/UserAPI'
 
 export default function useUsers() {
+  const [keyword, setKeyword] = useState('')
+
   const [users, setUsers] = useState()
   useEffect(() => {
     async function updatePosts() {
-      const { data } = await getUsers()
+      const { data } = await getUsers(keyword.toLowerCase())
       setUsers(data)
     }
     updatePosts()
-  }, [])
+  }, [keyword])
   return {
     users,
-    setUsers,
+    keyword,
+    setKeyword,
   }
 }
