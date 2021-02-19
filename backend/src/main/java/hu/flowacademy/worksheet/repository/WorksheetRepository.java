c1469ee7166072f8eb585537d776cb9f2af662d6
Mara Patrik <marapatgab@gmail.com>
2021-02-19 13:29:05 +0100

    worksheet_spec_wip

--- backend/src/main/java/hu/flowacademy/worksheet/repository/WorksheetRepository.java
+++ backend/src/main/java/hu/flowacademy/worksheet/repository/WorksheetRepository.java
@@ -2,12 +2,12 @@ package hu.flowacademy.worksheet.repository;
 
 import hu.flowacademy.worksheet.entity.Worksheet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
-public interface WorksheetRepository extends JpaRepository<Worksheet, String> {
-    List<Worksheet>findByCreatedAtBetween(LocalDateTime minTime, LocalDateTime maxTime);
+public interface WorksheetRepository extends JpaRepository<Worksheet, String>, JpaSpecificationExecutor<Worksheet> {
 }
