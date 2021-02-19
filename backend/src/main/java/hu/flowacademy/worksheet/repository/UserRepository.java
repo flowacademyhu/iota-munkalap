f2306f9ffa2aed7dd1bb221dcfea5b52be5d250c
gyanyim <gyanyi.miklos@gmail.com>
2021-02-19 10:14:09 +0100

    Delete the unused method in UserRepository.

--- backend/src/main/java/hu/flowacademy/worksheet/repository/UserRepository.java
+++ backend/src/main/java/hu/flowacademy/worksheet/repository/UserRepository.java
@@ -1,8 +1,6 @@
 package hu.flowacademy.worksheet.repository;
 
 import hu.flowacademy.worksheet.entity.User;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
@@ -11,7 +9,6 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
-    Page<User> findByEmailLikeIgnoreCaseOrFirstNameLikeIgnoreCaseOrLastNameLikeIgnoreCase(String emailPart, String firstNamePart, String lastNamePart, Pageable pageable);
 
     Optional<User> findFirstByEmail(String email);
 }
