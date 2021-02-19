4b37faf93a6c31a74c5fb9c19b97af4554cd7e8f
Mara Patrik <marapatgab@gmail.com>
2021-02-19 08:59:47 +0100

    Added query method to filter worksheetRepo by Status

--- backend/src/main/java/hu/flowacademy/worksheet/repository/WorksheetRepository.java
+++ backend/src/main/java/hu/flowacademy/worksheet/repository/WorksheetRepository.java
@@ -1,9 +1,13 @@
 package hu.flowacademy.worksheet.repository;
 
 import hu.flowacademy.worksheet.entity.Worksheet;
+import hu.flowacademy.worksheet.enumCustom.WorksheetStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface WorksheetRepository extends JpaRepository<Worksheet, String> {
+    List<Worksheet> findByWorksheetStatusLike(WorksheetStatus status);
 }
