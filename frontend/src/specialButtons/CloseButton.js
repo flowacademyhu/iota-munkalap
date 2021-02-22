3b0e18afa5d7cf31e2fee758b81e744b2d07a253
Aniko <s.aniko9424@gmail.com>
2021-02-22 14:58:13 +0100

    Change special buttons size and color

--- frontend/src/specialButtons/CloseButton.js
+++ frontend/src/specialButtons/CloseButton.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { XSquareFill } from 'react-bootstrap-icons'
 
-function CloseButton() {
-  return <XSquareFill />
+function CloseButton({ onClick }) {
+  return <XSquareFill onClick={onClick} className="editIcon closeIcon" />
 }
 
 export default CloseButton
--- frontend/src/specialButtons/EditButton.js
+++ frontend/src/specialButtons/EditButton.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { PencilSquare } from 'react-bootstrap-icons'
 
 function EditButton() {
-  return <PencilSquare />
+  return <PencilSquare className="editIcon" />
 }
 
 export default EditButton
