f611d570684278afb75e3cf8fb3047ba2e26ca36
gyanyim <gyanyi.miklos@gmail.com>
2021-02-22 10:47:41 +0100

    Added PartnerDTO datas.

--- backend/src/main/java/hu/flowacademy/worksheet/dto/PartnerDTO.java
+++ backend/src/main/java/hu/flowacademy/worksheet/dto/PartnerDTO.java
@@ -1,4 +1,33 @@
 package hu.flowacademy.worksheet.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PartnerDTO {
+
+    private String partnerEmail;
+    private String telefon;
+    private String nev;
+    private String rovidNev;
+    private String adoszam;
+    private Integer kAdoszamTipus;
+    private String bankszamlaszam;
+    private String szamlazasiCimOrszagKod;
+    private String szamlazasiCimOrszagNev;
+    private String szamlazasiCimMegyeNev;
+    private String szamlazasiCimIranyitoszam;
+    private String szamlazasiCimTelepulesNev;
+    private String szamlazasiCimKerulet;
+    private String szamlazasiCimKozteruletNev;
+    private String szamlazasiCimKozteruletJellegNev;
+    private String szamlazasiCimHazszam;
+    private String szamlazasiCimEpulet;
+    private String szamlazasiCimLepcsohaz;
+    private String szamlazasiCimSzint;
+    private String szamlazasiCimAjto;
+    private String szamlazasiCimHrsz;
 }
