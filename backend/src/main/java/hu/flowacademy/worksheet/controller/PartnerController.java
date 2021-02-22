9ff1141dfd083379a75aa4e6308e9f6a48e4266c
gyanyim <gyanyi.miklos@gmail.com>
2021-02-22 12:22:54 +0100

    Added PartnerController but is not ended.

--- backend/src/main/java/hu/flowacademy/worksheet/controller/PartnerController.java
+++ backend/src/main/java/hu/flowacademy/worksheet/controller/PartnerController.java
@@ -1,11 +1,52 @@
 package hu.flowacademy.worksheet.controller;
 
+import hu.flowacademy.worksheet.dto.PartnerDTO;
+import hu.flowacademy.worksheet.entity.Partner;
+import hu.flowacademy.worksheet.exception.ValidationException;
+import hu.flowacademy.worksheet.service.PartnerService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.security.RolesAllowed;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api")
 public class PartnerController {
+
+    private PartnerService partnerService;
+
+    @PostMapping("/partners")
+    @ResponseStatus(HttpStatus.CREATED)
+    @RolesAllowed({"admin", "user"})
+    public Partner createPartner(@RequestBody PartnerDTO partnerDTO) throws ValidationException {
+        Partner partner
+    }
+
 }
+
+/*  @PostMapping("/worksheets")
+    @ResponseStatus(HttpStatus.CREATED)
+    @RolesAllowed({"admin", "user"})
+    public Worksheet createWorksheet(@RequestBody WorksheetDTO worksheetDTO) throws ValidationException {
+        Worksheet worksheet = Worksheet.builder()
+                .partnerId(worksheetDTO.getPartnerId())
+                .typeOfWork(worksheetDTO.getTypeOfWork())
+                .customTypeOfWork(worksheetDTO.getCustomTypeOfWork())
+                .assetSettlement(worksheetDTO.getAssetSettlement())
+                .workingTimeAccounting(worksheetDTO.getWorkingTimeAccounting())
+                .numberOfEmployees(worksheetDTO.getNumberOfEmployees())
+                .overheadHour(worksheetDTO.getOverheadHour())
+                .deliveryKm(worksheetDTO.getDeliveryKm())
+                .accountSerialNumber(worksheetDTO.getAccountSerialNumber())
+                .description(worksheetDTO.getDescription())
+                .usedMaterial(worksheetDTO.getUsedMaterial())
+                .typeOfPayment(worksheetDTO.getTypeOfPayment())
+                .createdAt(worksheetDTO.getCreatedAt())
+                .workerSignature(worksheetDTO.getWorkerSignature())
+                .proofOfEmployment(worksheetDTO.getProofOfEmployment())
+                .worksheetStatus(worksheetDTO.getWorksheetStatus())
+                .build();
+        return worksheetService.saveWorksheet(worksheet);
+    }*/
