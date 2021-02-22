9d7e11e4296ee1976bdacd83496371d0b40e1c9c
Daniel Varsas <daniel.varsas@gmail.com>
2021-02-22 15:53:18 +0100

    Remove unnecessary div

--- frontend/src/LoadingScreen.js
+++ frontend/src/LoadingScreen.js
@@ -3,10 +3,8 @@ import load from './img/Pulse-1s-200px.gif'
 
 function LoadingScreen() {
   return (
-    <div id="row">
-      <div className="d-flex justify-content-center">
-        <img alt="Loading..." src={load} />
-      </div>
+    <div className="d-flex justify-content-center">
+      <img alt="Loading..." src={load} />
     </div>
   )
 }
