ad91098f1c19df51a0d711d2bfd65b55ba1aa8a3
Aniko <s.aniko9424@gmail.com>
2021-02-22 16:36:10 +0100

    Put decoding to try-catch

--- frontend/src/hooks/useCurrentUser.js
+++ frontend/src/hooks/useCurrentUser.js
@@ -2,18 +2,23 @@ import useToken from './useToken'
 import jwt from 'jwt-simple'
 
 function useCurrentUser() {
-  const { token } = useToken()
-  const decodedToken = jwt.decode(token, false, 'RS256')
+  const { token, setToken } = useToken()
+  try {
+    const decodedToken = jwt.decode(token, false, 'RS256')
 
-  const name = decodedToken?.family_name + ' ' + decodedToken?.given_name
-  const email = decodedToken?.email
-  const isAdmin = decodedToken?.resource_access.worksheetclient.roles.includes(
-    'admin'
-  )
-  return {
-    name,
-    email,
-    isAdmin,
+    const name = decodedToken?.family_name + ' ' + decodedToken?.given_name
+    const email = decodedToken?.email
+    const isAdmin = decodedToken?.resource_access.worksheetclient.roles.includes(
+      'admin'
+    )
+    return {
+      name,
+      email,
+      isAdmin,
+    }
+  } catch (error) {
+    setToken('')
+    window.location.reload()
   }
 }
 
