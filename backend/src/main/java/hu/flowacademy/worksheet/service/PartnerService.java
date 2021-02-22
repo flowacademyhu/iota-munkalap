582a53b2afd717d647224e728fae9a97496bf367
gyanyim <gyanyi.miklos@gmail.com>
2021-02-22 11:02:12 +0100

    Added PartnerService with annotations.

---
+++ backend/src/main/java/hu/flowacademy/worksheet/service/PartnerService.java
@@ -0,0 +1,12 @@
+package hu.flowacademy.worksheet.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class PartnerService {
+}
