e7b878a013640e9898f4f2add78432ec1cea7449
Aniko <s.aniko9424@gmail.com>
2021-02-23 15:02:54 +0100

    Create AdminRoute.js file

--- frontend/src/Routing.js
+++ frontend/src/Routing.js
@@ -1,19 +1,49 @@
 import React from 'react'
-import { Switch } from 'react-router-dom'
-import useCurrentUser from './hooks/useCurrentUser'
-import RoutesOnlyForAdmin from './RoutesOnlyForAdmin'
-import RoutesForEveryOne from './RoutesForEveryOne'
+import { Switch, Route, Redirect } from 'react-router-dom'
+import TableListOfEmployees from './employees/TableListOfEmployees'
+import CreateEmployee from './employees/CreateEmployee'
+import UpdateEmployee from './employees/UpdateEmployee'
+import TableListOfWorkSheets from './worksheets/TableListOfWorkSheets'
+import TableListofPartners from './partners/TableListofPartners'
+import CreateWorkSheet from './worksheets/CreateWorkSheet'
+import LoadingScreen from './LoadingScreen'
+import AdminRoute from './AdminRoute'
 
 function Routing() {
-  const { isAdmin } = useCurrentUser()
   return (
     <div className="col-12">
       <Switch>
-        {isAdmin && <RoutesOnlyForAdmin />}
-        <RoutesForEveryOne />
+        <AdminRoute path="/employees/new">
+          <CreateEmployee />
+        </AdminRoute>
+        <AdminRoute path="/employees/update/:id">
+          <UpdateEmployee />
+        </AdminRoute>
+        <AdminRoute exact path="/employees">
+          <TableListOfEmployees />
+        </AdminRoute>
+        <Route path="/partners">
+          <TableListofPartners />
+        </Route>
+        <Route path="/worksheets/new">
+          <CreateWorkSheet />
+        </Route>
+        <Route path="/worksheets/update/:id">
+          <UpdateWorkSheet />
+        </Route>
+        <Route exact path="/worksheets">
+          <TableListOfWorkSheets />
+        </Route>
+        <Route path="/">
+          <Redirect to="/partners" />
+        </Route>
       </Switch>
     </div>
   )
+}
+
+function UpdateWorkSheet() {
+  return <LoadingScreen />
 }
 
 export default Routing
