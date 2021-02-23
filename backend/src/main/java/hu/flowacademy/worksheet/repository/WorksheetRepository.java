05cf76477de8bdf1aff973293a96241cc4d96d20
Mara Patrik <marapatgab@gmail.com>
2021-02-23 10:16:10 +0100

    removed unnecessary import and lines

--- backend/src/main/java/hu/flowacademy/worksheet/repository/WorksheetRepository.java
+++ backend/src/main/java/hu/flowacademy/worksheet/repository/WorksheetRepository.java
@@ -8,8 +8,6 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface WorksheetRepository extends JpaRepository<Worksheet, String>, JpaSpecificationExecutor<Worksheet> {
     @Modifying
