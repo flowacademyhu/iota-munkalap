853c1bc5336889e3699979ed316787e04affaac0
Meli <bodzamelani@gmail.com>
2021-02-23 17:15:57 +0100

    added <td></td>

--- frontend/src/worksheets/TableListOfWorkSheets.js
+++ frontend/src/worksheets/TableListOfWorkSheets.js
@@ -66,7 +66,9 @@ export default function TableListOfWorkSheets() {
                 ))
               ) : (
                 <tr colspan="5">
-                  <LoadingScreen />
+                  <td>
+                    <LoadingScreen />
+                  </td>
                 </tr>
               )}
             </tbody>
