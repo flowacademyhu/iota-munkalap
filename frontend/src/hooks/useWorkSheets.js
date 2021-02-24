7b98040951fae47dd579e2ea9e2b9424be1dd834
Daniel Varsas <daniel.varsas@gmail.com>
2021-02-24 10:21:48 +0100

    Add default value for status

--- frontend/src/hooks/useWorkSheets.js
+++ frontend/src/hooks/useWorkSheets.js
@@ -2,16 +2,19 @@ import { useState, useEffect, useCallback } from 'react'
 import { getWorkSheets } from '../api/WorkSheetAPI'
 
 export default function useWorkSheets() {
-  const [status, setStatus] = useState('')
+  const [status, setStatus] = useState('CREATED')
 
   const [workSheets, setWorkSheets] = useState()
 
-  const updateWorkSheets = useCallback(async function () {
-    const { data } = await getWorkSheets()
-    if (data) {
-      setWorkSheets(data)
-    }
-  }, [])
+  const updateWorkSheets = useCallback(
+    async function () {
+      const { data } = await getWorkSheets(status)
+      if (data) {
+        setWorkSheets(data)
+      }
+    },
+    [status]
+  )
 
   useEffect(() => {
     updateWorkSheets()
