6012c71e81e13d1812be1d567ce4cd5a5f5ceb0e
Mara Patrik <marapatgab@gmail.com>
2021-02-24 19:03:01 +0100

    class for login and authorization

---
+++ backend/src/test/java/hu/flowacademy/worksheet/helper/TestHelper.java
@@ -0,0 +1,24 @@
+package hu.flowacademy.worksheet.helper;
+
+import hu.flowacademy.worksheet.dto.UserOperationDTO;
+import io.restassured.http.ContentType;
+import io.restassured.http.Header;
+import org.keycloak.representations.AccessTokenResponse;
+
+import static io.restassured.RestAssured.*;
+
+public class TestHelper {
+    public static Header getAuthorization(String token) {
+        return new Header("Authorization", "Bearer " + token);
+    }
+
+    public static String adminLogin() {
+        return given()
+                .body(UserOperationDTO.builder().email("superadmin@superadmin.hu").password("superadmin").build())
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/api/login")
+                .then()
+                .extract().body().as(AccessTokenResponse.class).getToken();
+    }
+}
