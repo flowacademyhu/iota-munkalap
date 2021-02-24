2fe2f06aa8ca3f66dbd231a1b7fc2c5684e36749
gyanyim <gyanyi.miklos@gmail.com>
2021-02-24 16:42:14 +0100

    Added dependencies in pom.xml and setup a PartnerControllerTest.

---
+++ backend/src/test/java/hu/flowacademy/worksheet/controller/PartnerControllerTest.java
@@ -0,0 +1,23 @@
+package hu.flowacademy.worksheet.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class PartnerControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    public static void method() {
+
+    }
+
+}
