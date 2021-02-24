c6c9362bcbb18e5c746c15580445b1124f676e55
Aniko <s.aniko9424@gmail.com>
2021-02-24 10:01:43 +0100

    Change close button's view

--- frontend/src/specialButtons/CloseButton.js
+++ frontend/src/specialButtons/CloseButton.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { XSquareFill } from 'react-bootstrap-icons'
+import { FileLockFill } from 'react-bootstrap-icons'
 
 function CloseButton({ onClick }) {
-  return <XSquareFill onClick={onClick} className="editIcon closeIcon" />
+  return <FileLockFill onClick={onClick} className="editIcon closeIcon" />
 }
 
 export default CloseButton
