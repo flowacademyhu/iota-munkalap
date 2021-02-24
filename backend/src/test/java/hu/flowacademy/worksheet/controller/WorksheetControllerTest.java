b5a224b546ba28a3235a93455c4fba84a07ac2fa
Mara Patrik <marapatgab@gmail.com>
2021-02-24 14:47:05 +0100

    temp

--- backend/src/test/java/hu/flowacademy/worksheet/controller/WorksheetControllerTest.java
+++ backend/src/test/java/hu/flowacademy/worksheet/controller/WorksheetControllerTest.java
@@ -1,7 +1,29 @@
 package hu.flowacademy.worksheet.controller;
+import hu.flowacademy.worksheet.dto.WorksheetDTO;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static io.restassured.RestAssured.*;
 
 class WorksheetControllerTest {
+
+    @Autowired
+    private WorksheetDTO worksheetDTO;
+
+    @BeforeAll
+    public static void beforeAll() {
+        RestAssured.port = 8081;
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.rootPath = "/api";
+    }
+
+    @Test
+    public void successfulCreationReturns200() {
+        given()
+                .body(worksheetDTO)
+                .when().post("/worksheets").then().statusCode(200);
+    }
 
 }
