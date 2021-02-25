0beacf89b4efdd5b18096b344f8de2fa22ff551e
Aniko <s.aniko9424@gmail.com>
2021-02-25 19:12:10 +0100

    Add placeholderText

--- frontend/src/CalendarDropDown.js
+++ frontend/src/CalendarDropDown.js
@@ -2,14 +2,14 @@ import React from 'react'
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
 
-function CalendarDropDown({ date, setDate }) {
+function CalendarDropDown({ date, setDate, placeholderText }) {
   return (
     <DatePicker
       selected={date}
       onChange={(date) => setDate(date)}
       dateFormat="yyyy.MM.dd"
       disabledKeyboardNavigation
-      //  placeholderText={placeholderText}
+      placeholderText={placeholderText}
       className="mr-5 mb-5"
     />
   )
