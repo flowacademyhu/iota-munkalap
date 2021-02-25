636a7b87916d2ce02f40e2cb2da39df5cfeb22a9
nomansnick <marapatgab@gmail.com>
2021-02-25 14:47:33 +0100

    user_search fix (lowercase added, criteria, service)

--- backend/src/main/java/hu/flowacademy/worksheet/service/filter/UserSpecification.java
+++ backend/src/main/java/hu/flowacademy/worksheet/service/filter/UserSpecification.java
@@ -1,20 +1,21 @@
 package hu.flowacademy.worksheet.service.filter;
 import hu.flowacademy.worksheet.entity.User;
 import org.springframework.data.jpa.domain.Specification;
+
 import java.util.Optional;
 public class UserSpecification {
     public static Specification<User> firstnameLastnameEmailContains(Optional<String> q) {
         return (root, query, criteriaBuilder) -> q
                 .map(
-                        // TODO check ignore case
                         searchParam -> criteriaBuilder.or(
-                                criteriaBuilder.like(root.get("firstName"), searchParam),
-                                criteriaBuilder.like(root.get("lastName"), searchParam),
-                                criteriaBuilder.like(root.get("email"), searchParam)
+                                criteriaBuilder.like(criteriaBuilder.lower(root.get("firstName")), searchParam),
+                                criteriaBuilder.like(criteriaBuilder.lower(root.get("lastName")), searchParam),
+                                criteriaBuilder.like(criteriaBuilder.lower(root.get("email")), searchParam)
                         )
                 )
                 .orElse(null);
     }
+
     public static Specification<User> enabled(Optional<Boolean> status) {
         return (root, query, criteriaBuilder) -> status
                 .map(enabled -> criteriaBuilder.equal(root.get("enabled"), enabled))
@@ -25,7 +26,7 @@ public class UserSpecification {
         return Specification
                 .where(enabled(status))
                 .and(firstnameLastnameEmailContains(
-                        q.map(searchPart -> "%" + searchPart.replaceAll("[aáeéiíoóöőuúüű]", "_") + "%")
+                        q.map(searchPart -> "%" + searchPart.toLowerCase().replaceAll("[aáeéiíoóöőuúüű]", "_") + "%")
                         )
                 );
     }
