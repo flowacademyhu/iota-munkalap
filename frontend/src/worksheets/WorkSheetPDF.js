d19ec48792746867b37e43b9696c9eb0f6e27d81
Meli <bodzamelani@gmail.com>
2021-02-25 21:18:36 +0100

    deleted comments and unused impoerts, variables

--- frontend/src/worksheets/WorkSheetPDF.js
+++ frontend/src/worksheets/WorkSheetPDF.js
@@ -1,15 +1,12 @@
-import logoString from './LogoForPdf'
+import { LOGO_STRING } from './LogoForPdf'
 import pdfMake from 'pdfmake/build/pdfmake'
 import vfsFonts from 'pdfmake/build/vfs_fonts'
 import {
   typeOfWork,
-  status,
   assetSettlement,
-  workingTimeAccounting,
   typeOfPayment,
 } from '../TranslationForWorkSheet'
 import SignaturePad from 'signature_pad'
-import imageToBase64 from 'image-to-base64'
 
 function renderSvg(param) {
   const canvasSignature = document.createElement('canvas')
@@ -21,8 +18,6 @@ function renderSvg(param) {
   const data = svgSignature.split(',')[1]
   return atob(data)
 }
-
-let logoBase64 = logoString
 
 const acknowledge =
   'A munkavégzést igazoló aláírásával a fent megjelölt munka teljesítését elismeri, az üzemelő rendszert átveszi.'
@@ -54,7 +49,7 @@ function WorkSheetPDF(worksheet) {
             [
               [
                 {
-                  image: logoBase64,
+                  image: LOGO_STRING,
                   width: 150,
                 },
               ],
