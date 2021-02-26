bb9b8a373cd047f169d7c181add99c6b3216ed42
Daniel Varsas <daniel.varsas@gmail.com>
2021-02-26 16:41:56 +0100

    Create tableContent.js

---
+++ frontend/src/worksheets/tableContents.js
@@ -0,0 +1,37 @@
+
+///eleje
+content: [
+    {
+      text: 'Munkalap',
+      style: 'header',
+      alignment: 'right',
+    },
+    ///eleje
+    //tartalom///
+
+    //tartalom///
+    ///vége
+
+    styles: {
+        header: {
+          fontSize: 18,
+          bold: true,
+          margin: [0, 0, 0, 10],
+        },
+        subheader: {
+          fontSize: 16,
+          bold: true,
+          margin: [0, 10, 0, 5],
+        },
+        tableExample: {
+          margin: [0, 5, 0, 15],
+        },
+        tableHeader: {
+          bold: true,
+          fontSize: 13,
+          color: 'black',
+        },
+      },
+      defaultStyle: {},
+    }
+    //vége
