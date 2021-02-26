7df6c3fe801f169ff17fff6e991d16a55ae2b3c7
Mara Patrik <marapatgab@gmail.com>
2021-02-26 10:36:55 +0100

    partnerSpec wip

--- backend/src/main/java/hu/flowacademy/worksheet/service/filter/PartnerSpecification.java
+++ backend/src/main/java/hu/flowacademy/worksheet/service/filter/PartnerSpecification.java
@@ -1,34 +1,26 @@
 package hu.flowacademy.worksheet.service.filter;
 
-import hu.flowacademy.worksheet.entity.User;
+import hu.flowacademy.worksheet.entity.Partner;
 import org.springframework.data.jpa.domain.Specification;
 
 import java.util.Optional;
 
 public class PartnerSpecification {
-    public static Specification<User> firstnameLastnameEmailContains(Optional<String> q) {
+    public static Specification<Partner> nevOrAdoszamContains(Optional<String> q) {
         return (root, query, criteriaBuilder) -> q
                 .map(
-                        // TODO check ignore case
                         searchParam -> criteriaBuilder.or(
-                                criteriaBuilder.like(root.get("firstName"), searchParam),
-                                criteriaBuilder.like(root.get("lastName"), searchParam),
-                                criteriaBuilder.like(root.get("email"), searchParam)
+                                criteriaBuilder.like(criteriaBuilder.lower(root.get("nev")), searchParam),
+                                criteriaBuilder.like(criteriaBuilder.lower(root.get("adoszam")), searchParam)
                         )
                 )
                 .orElse(null);
     }
-    public static Specification<User> enabled(Optional<Boolean> status) {
-        return (root, query, criteriaBuilder) -> status
-                .map(enabled -> criteriaBuilder.equal(root.get("enabled"), enabled))
-                .orElse(null);
-    }
 
-    public static Specification<User> buildSpecification(Optional<Boolean> status, Optional<String> q) {
+    public static Specification<Partner> buildSpecification(Optional<String> q) {
         return Specification
-                .where(enabled(status))
-                .and(firstnameLastnameEmailContains(
-                        q.map(searchPart -> "%" + searchPart.replaceAll("[aáeéiíoóöőuúüű]", "_") + "%")
+                .where(nevOrAdoszamContains(
+                        q.map(searchPart -> "%" + searchPart.toLowerCase().replaceAll("[aáeéiíoóöőuúüű]", "_") + "%")
                         )
                 );
     }
