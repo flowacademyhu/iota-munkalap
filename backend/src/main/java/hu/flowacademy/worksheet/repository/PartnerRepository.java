48f65c97e5277bad58a6024ea9e06a884a4e2d86
Mara Patrik <marapatgab@gmail.com>
2021-02-27 08:14:36 +0100

    removed unused method

--- backend/src/main/java/hu/flowacademy/worksheet/repository/PartnerRepository.java
+++ backend/src/main/java/hu/flowacademy/worksheet/repository/PartnerRepository.java
@@ -5,9 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface PartnerRepository extends JpaRepository<Partner, String>, JpaSpecificationExecutor<Partner> {
-    Optional<Partner> findFirstByNev(String nev);
 }
