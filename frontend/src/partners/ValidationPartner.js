0b71d3c28039ea23f1fb6718c24f8e6186b21f4c
Meli <bodzamelani@gmail.com>
2021-02-27 10:10:46 +0100

    changed requireds text

--- frontend/src/partners/ValidationPartner.js
+++ frontend/src/partners/ValidationPartner.js
@@ -5,20 +5,20 @@ export default function schema() {
   const schema = yup.object().shape({
     partnerEmail: yup
       .string()
-      .required('E-mail megadása kötelező!')
+      .required('Kötelező mező!')
       .email('Nem megfelelő email cím!'),
     telefon: yup
       .string()
-      .required('Telefon megadása kötelező')
+      .required('Kötelező mező!')
       .matches(/([0-9])/, 'Nem megfelelő formátum!'),
     megrendeloTipusa: yup.string().required('Kötelező mező!'),
-    nev: yup.string().required('Név megadása kötelező!'),
+    nev: yup.string().required('Kötelező mező!'),
     rovidNev: yup.string(),
     adoszam: yup.string().when('megrendeloTipusa', {
       is: MEGRENDELO_TIPUSA.LEGAL,
       then: yup
         .string()
-        .required('Adószám megadása kötelező!')
+        .required('Kötelező mező!')
         .length(8, 'Az adószám 8 számjegyet kell tartalmazzon!')
         .matches(/([0-9]{8})/),
       otherwise: yup.string(),
@@ -27,7 +27,7 @@ export default function schema() {
       is: MEGRENDELO_TIPUSA.LEGAL,
       then: yup
         .number()
-        .required('kAdószám megadása kötelező!')
+        .required('Kötelező mező!')
         .min(1, 'A kadószám 1 és 5 közötti szám!')
         .max(5, 'A kadószám 1 és 5 közötti szám!'),
       otherwise: yup.number(),
@@ -38,23 +38,15 @@ export default function schema() {
         /([0-9]{8})-([0-9]{8})/ || /([0-9]{8})-([0-9]{8})-([0-9]{8})/,
         'Nem megfelelő bankszámlaszám! Bankszámlaszám formátuma: 12345678-12345678 vagy 12345678-12345678-12345678'
       ),
-    szamlazasiCimOrszagKod: yup
-      .string()
-      .required('Országkód megadása kötelező!'),
-    szamlazasiCimOrszagNev: yup
-      .string()
-      .required('Országnév megadása kötelező!'),
-    szamlazasiCimMegyeNev: yup.string().required('Megye megadása kötelező!'),
-    szamlazasiCimIranyitoszam: yup
-      .string()
-      .required('Irányítószám megadása kötelező'),
-    szamlazasiCimTelepulesNev: yup
-      .string()
-      .required('Település megadása kötelező'),
+    szamlazasiCimOrszagKod: yup.string().required('Kötelező mező!'),
+    szamlazasiCimOrszagNev: yup.string().required('Kötelező mező!'),
+    szamlazasiCimMegyeNev: yup.string().required('Kötelező mező!'),
+    szamlazasiCimIranyitoszam: yup.string().required('Kötelező mező!'),
+    szamlazasiCimTelepulesNev: yup.string().required('Kötelező mező!'),
     szamlazasiCimKerulet: yup.string(),
-    szamlazasiCimKozteruletNev: yup.string().required(),
-    szamlazasiCimKozteruletJellegNev: yup.string().required(),
-    szamlazasiCimHazszam: yup.string().required(),
+    szamlazasiCimKozteruletNev: yup.string().required('Kötelező mező!'),
+    szamlazasiCimKozteruletJellegNev: yup.string().required('Kötelező mező!'),
+    szamlazasiCimHazszam: yup.string().required('Kötelező mező!'),
     szamlazasiCimEpulet: yup.string(),
     szamlazasiCimLepcsohaz: yup.string(),
     szamlazasiCimSzint: yup.string(),
