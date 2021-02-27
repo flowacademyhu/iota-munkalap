20d9f1480b70b10a9a259c37f3ae98beb8cc8ff4
Meli <bodzamelani@gmail.com>
2021-02-27 11:20:54 +0100

    deleted comment

--- frontend/src/RadioInput.js
+++ frontend/src/RadioInput.js
@@ -1,7 +1,4 @@
 import classNames from 'classnames'
-
-// ne legyena arrow function,  usefield legyen a field, többit propsbol consttal szedem ki,
-// mi az onchange,-> h lehessen változtatni,  onBlur? -> nincs jelentősége
 function RadioInput(props) {
   const { field, id, label } = props
   const { name, value, onChange } = field
