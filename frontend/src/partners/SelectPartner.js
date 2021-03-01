7f05ad57ab9455ca5ed6d26982e01dbba9444630
Aniko <s.aniko9424@gmail.com>
2021-03-01 22:39:28 +0100

    Show error message if the input is touched but no selected partner

--- frontend/src/partners/SelectPartner.js
+++ frontend/src/partners/SelectPartner.js
@@ -1,20 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Select from 'react-select'
+import { useField } from 'formik'
 
-function SelectPartner({ setFieldValue, options, name }) {
+function SelectPartner({ setFieldValue, options, ...props }) {
+  const [showError, setShowError] = useState(false)
+  const { error } = useField(props)[1]
+  const { name } = props
+
   const handleChange = (partner) => {
     setFieldValue('partnerId', partner.partnerId)
   }
 
+  const handleBlur = () => {
+    setShowError(true)
+  }
+
   return (
-    <>
+    <div className="form-group">
       <label htmlFor={name}>Partner</label>
       <Select
+        id={name}
         options={options}
         onChange={handleChange}
+        onBlur={handleBlur}
         placeholder="Partner neve"
+        className={`${showError ? 'is-invalid' : ''}`}
       />
-    </>
+      {showError && <div className="invalid-feedback">{error}</div>}
+    </div>
   )
 }
 
