0f09093e8b3f82726b9f4d7730523165c669b737
BigG <gabszon002@gmail.com>
2021-03-01 11:21:53 +0100

    changed validation again

--- frontend/src/employees/ValidationEmplyee.js
+++ frontend/src/employees/ValidationEmplyee.js
@@ -1,33 +1,28 @@
 import * as yup from 'yup'
 
 export function schema(isCreate) {
-  const schema = yup.object().shape({
+  const passwordValidation = isCreate
+    ? {
+        password: yup
+          .string()
+          .required('A jelszó kötelező!')
+          .matches(
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/,
+            'A jelszó minimum 8 karakter hosszú, tartalmaznia kell kis- és nagybetűt, valamint számot!'
+          ),
+        confirmPassword: yup
+          .string()
+          .required('Add meg a jelszót még egyszer!')
+          .oneOf([yup.ref('password')], 'A két jelszó nem egyezik meg!'),
+      }
+    : {}
+  return yup.object().shape({
     lastName: yup.string().required('A vezetéknév kötelező!'),
     firstName: yup.string().required('A keresztnév kötelező!'),
     email: yup
       .string()
       .required('Az email kötelező!')
       .email('Nem megfelelő email cím!'),
+    ...passwordValidation,
   })
-
-  const regSchema = yup.object().shape({
-    lastName: yup.string().required('A vezetéknév kötelező!'),
-    firstName: yup.string().required('A keresztnév kötelező!'),
-    email: yup
-      .string()
-      .required('Az email kötelező!')
-      .email('Nem megfelelő email cím!'),
-    password: yup
-      .string()
-      .required('A jelszó kötelező!')
-      .matches(
-        /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/,
-        'A jelszó minimum 8 karakter hosszú, tartalmaznia kell kis- és nagybetűt, valamint számot!'
-      ),
-    confirmPassword: yup
-      .string()
-      .required('Add meg a jelszót még egyszer!')
-      .oneOf([yup.ref('password')], 'A két jelszó nem egyezik meg!'),
-  })
-  return isCreate ? regSchema : schema
 }
