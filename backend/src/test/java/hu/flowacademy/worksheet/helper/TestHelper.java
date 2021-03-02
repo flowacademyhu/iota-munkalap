029db314e26fe18e0c494fae5a46fc04916affba
Daniel Varsas <daniel.varsas@gmail.com>
2021-03-02 02:28:34 +0100

    Add enabled var to TestHelper

--- backend/src/test/java/hu/flowacademy/worksheet/helper/TestHelper.java
+++ backend/src/test/java/hu/flowacademy/worksheet/helper/TestHelper.java
@@ -38,6 +38,7 @@ public class TestHelper {
     private static final String SZINT = "II.";
     private static final String AJTO = "11";
     private static final String HRSZ = "0123-4567-8901";
+    private static final Boolean ENABLED = true;
 
     public static Header getAuthorization(String token) {
         return new Header("Authorization", "Bearer " + token);
@@ -90,6 +91,7 @@ public class TestHelper {
                 .body("szamlazasiCimSzint", equalTo(SZINT))
                 .body("szamlazasiCimAjto", equalTo(AJTO))
                 .body("szamlazasiCimHrsz", equalTo(HRSZ))
+                .body("aktiv", equalTo(ENABLED))
                 .extract().body().as(Partner.class);
     }
 
@@ -117,6 +119,7 @@ public class TestHelper {
                 .szamlazasiCimSzint(SZINT)
                 .szamlazasiCimAjto(AJTO)
                 .szamlazasiCimHrsz(HRSZ)
+                .enabled(ENABLED)
                 .build();
     }
 }
