c600e7847d19770f73ed8153b8c89b8b200fdfdb
Aniko <s.aniko9424@gmail.com>
2021-03-03 15:57:23 +0100

    Add new div for row class

--- frontend/src/worksheets/DateRangeFilter.js
+++ frontend/src/worksheets/DateRangeFilter.js
@@ -7,20 +7,22 @@ function DateRangeFilter({ startDate, onStartDate, endDate, onEndDate }) {
     <>
       <div>Szűrés dátum szerint:</div>
       <div className="row align-items-center">
-        <div className="col-md-10 row">
-          <div className="col-xl-5 mx-3 d-flex justify-content-center my-1">
-            <CalendarDropDown
-              date={startDate}
-              setDate={onStartDate}
-              placeholderText="Intervallum kezdete"
-            />
-          </div>
-          <div className="col-xl-5 mx-3 d-flex justify-content-center my-1">
-            <CalendarDropDown
-              date={endDate}
-              setDate={onEndDate}
-              placeholderText="Intervallum vége"
-            />
+        <div className="col-md-10">
+          <div className="row">
+            <div className="col-xl-5 mx-3 d-flex justify-content-center my-1">
+              <CalendarDropDown
+                date={startDate}
+                setDate={onStartDate}
+                placeholderText="Intervallum kezdete"
+              />
+            </div>
+            <div className="col-xl-5 mx-3 d-flex justify-content-center my-1">
+              <CalendarDropDown
+                date={endDate}
+                setDate={onEndDate}
+                placeholderText="Intervallum vége"
+              />
+            </div>
           </div>
         </div>
         <div className="col-md-2 d-flex justify-content-center my-1">
