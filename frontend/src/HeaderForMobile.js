08edc689a95cc0c7f48350299f8ffb92cd4a2281
Aniko <s.aniko9424@gmail.com>
2021-03-03 09:38:08 +0100

    Create header for mobile

---
+++ frontend/src/HeaderForMobile.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import logo from './img/uj_logo.png'
+import Dropdown from 'react-bootstrap/Dropdown'
+import LogOut from './LogOut'
+import EmployeeData from './EmployeeData'
+import ToolsButton from './ToolsButton'
+
+function HeaderForMobile() {
+  return (
+    <>
+      <div className="row mt-2">
+        <div className="col p-0">
+          <img
+            className="col-auto logo p-0 m-0"
+            src={logo}
+            alt="ingo stop logo"
+          />
+        </div>
+
+        <Dropdown className="col text-right p-0">
+          <Dropdown.Toggle
+            className="bg-transparent border-light w-auto p-0"
+            id="dropdown-basic"
+          >
+            <ToolsButton />
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item>
+              <LogOut />
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+      <div className="col text-center h5 p-0">
+        <EmployeeData />
+      </div>
+    </>
+  )
+}
+
+export default HeaderForMobile
