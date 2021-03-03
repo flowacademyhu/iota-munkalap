03cfd60bf9b97f54d42add346b2a662737fd694c
Aniko <s.aniko9424@gmail.com>
2021-03-03 16:24:05 +0100

    Change td to Td

--- frontend/src/partners/TableListofPartners.js
+++ frontend/src/partners/TableListofPartners.js
@@ -54,8 +54,8 @@ export default function TableListofPartners() {
                           onClick={() => activate(partner.partnerId)}
                         />
                       )}
-                    </td>
-                  </tr>
+                    </Td>
+                  </Tr>
                 ))
               ) : (
                 <Tr>
