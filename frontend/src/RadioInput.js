ff945c6b4b72bdbef794108ec6290110dbe89f38
Meli <bodzamelani@gmail.com>
2021-03-04 13:11:06 +0100

    added state  and onChange to radioInput

--- frontend/src/RadioInput.js
+++ frontend/src/RadioInput.js
@@ -1,13 +1,22 @@
+import { useState } from 'react'
 import Form from 'react-bootstrap/Form'
 
-function RadioInput({ label, value, name }) {
+function RadioInput({ label, value, name, response }) {
+  const [selectedValue, setSelectedValue] = useState(value)
+
+  const handleChange = (e) => {
+    setSelectedValue(e.currentTarget.value)
+  }
+
   return (
     <Form.Check
       className="ml-4"
       type="radio"
       label={label}
       name={name}
-      value={value}
+      value={selectedValue}
+      defaultChecked={response && value === response}
+      onChange={(e) => handleChange(e)}
     />
   )
 }
